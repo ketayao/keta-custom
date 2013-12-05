7a825cf3ba5a891994b0c58112e665e899212f77
ketayao <ketayao@gmail.com>
2013-12-05 14:32:44 +0800

    添加SecurityUitls用户信息获取工具类。
    Signed-off-by: ketayao <ketayao@gmail.com>

---
+++ keta-custom-main/src/main/java/com/ketayao/utils/SecurityUtils.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.ketayao.utils;
+
+import org.apache.shiro.subject.Subject;
+
+import com.ketayao.ketacustom.entity.main.User;
+import com.ketayao.ketacustom.shiro.ShiroDbRealm.ShiroUser;
+
+/**
+ * @author KETAYAO
+ *
+ */
+public abstract class SecurityUtils {
+	public static User getLoginUser() {
+		return getShiroUser().getUser();
+	}
+	
+	public static ShiroUser getShiroUser() {
+		Subject subject = getSubject();
+		ShiroUser shiroUser = (ShiroUser)subject.getPrincipal();
+		
+		return shiroUser;
+	}
+
+	public static Subject getSubject() {
+		return org.apache.shiro.SecurityUtils.getSubject();
+	}
+}
